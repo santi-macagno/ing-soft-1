eb7b67f9739f9ce2c3441be04e3f596c27f890cd
Santiago Macagno <santiago.macagno@mi.unc.edu.ar>
2026-06-03 17:46:33 -0300

    chore: arreglo todo

--- tp_design/test/FakeLoggerJUnitTest.java
+++ tp_design/test/FakeLoggerJUnitTest.java
@@ -7,10 +7,10 @@ public class FakeLoggerJUnitTest {
     @Test
     public void recordsMessages() {
         FakeLogger logger = new FakeLogger();
-        logger.logInfo("test_error");
-        logger.logWarning("test_error");
-        logger.logError("test_error");
-        logger.logDebug("test_error");
+        logger.logInfo("hi");
+        logger.logWarning("w");
+        logger.logError("e");
+        logger.logDebug("d");
 
         List<String> msgs = logger.getMessages();
         assertEquals(4, msgs.size());
